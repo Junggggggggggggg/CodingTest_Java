e1537bf79323525edca742cde9b2a07e4f7747ff
슬기 <149359157+Junggggggggggggg@users.noreply.github.com>
2024-02-25 13:09:40 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.37 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int[] num_list) {
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        Arrays.sort(num_list);
+        
+        for( int i=5; i<num_list.length; i++) {
+            answer.add(num_list[i]);
+        }
+        
+        return answer;
+    }
+}
