f04818477198bf6993383ac0de82b94da8349d7b
슬기 <149359157+Junggggggggggggg@users.noreply.github.com>
2024-02-26 14:21:20 +0900

    [level 0] Title: 0 떼기, Time: 7.07 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String n_str) {
+        
+        int result = Integer.parseInt(n_str);
+        
+        String answer = result+ "";
+            
+        return answer;
+    }
+}
